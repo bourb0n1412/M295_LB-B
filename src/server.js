369b45b46669b3089edd69e8fb48698250b73bd1
bourb0n1412 <heikoho1@icloud.com>
2024-06-14 15:36:59 +0200

    Errorhandling implemented

--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
+// QUELLEN:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch?retiredLocale=de
+// http://expressjs.com/de/api.html#res.cookie
+
 const express = require('express');
 const session = require('express-session');
 const tasks = require('./data/tasks.json')
@@ -13,24 +17,32 @@ app.use(session({
 }));
 
 // Auth Controller
-// http://expressjs.com/de/api.html#res.cookie
 // Endpunkt, welcher die Credentials entgegennimmt, überprüft und ein Cookie zurück gibt (http://expressjs.com/de/api.html#res.cookie)
 app.post('/login', (req, res) => {
-    const email = req.headers.email;
-    const password = req.headers.password;
-    if (password === "m295") {
-        req.session.auth = true
-        res.cookie('auth', true, { httpOnly: true })
-        res.status(201).json({
-            authenticated: req.session.auth,
-            message: 'Login successful'
-        });
-    } else {
-        res.cookie('auth', false, { httpOnly: true })
-        res.status(401).json({
-            message: 'Unauthorized'
+    try {
+        const email = req.headers.email;
+        const password = req.headers.password;
+        if (password === "m295") {
+            req.session.auth = true
+            res.cookie('auth', true, { httpOnly: true })
+            res.status(201).json({
+                authenticated: req.session.auth,
+                message: 'Login successful'
+            });
+        } else {
+            res.cookie('auth', false, { httpOnly: true })
+            res.status(401).json({
+                message: 'Unauthorized'
+            });
+        }
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({
+            success: false,
+            message: 'Internal server error'
         });
     }
+
 });
 
 // Endpunkt, welcher das Cookie auf Gültigkeit überprüft und das Ergebnis zurück gibt
@@ -50,10 +62,19 @@ app.get('/verify', (req, res) => {
 
 // Endpunkt, welcher das mitgegebene Cookie als ungültig markiert
 app.delete('/logout', (req, res) => {
-    req.session.auth
-    req.session.auth = false
-    res.cookie('auth', false, { httpOnly: true })
-    res.status(204)
+    try {
+        req.session.auth
+        req.session.auth = false
+        res.cookie('auth', false, { httpOnly: true })
+        res.status(204)
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({
+            success: false,
+            message: 'Internal server error'
+        });
+    }
+
 })
 
 
@@ -61,90 +82,141 @@ app.delete('/logout', (req, res) => {
 // Task Controller
 // Endpunkt, welcher eine Liste aller Tasks zurück gibt
 app.get('/tasks', (req, res) => {
-    if (req.session.auth) {
-        res.status(200).json(tasks)
-    } else {
-        res.status(403).json({
-            message: "You don't have permsissions"
-        })
+    try {
+        if (req.session.auth) {
+            res.status(200).json(tasks)
+        } else {
+            res.status(403).json({
+                message: "You don't have permsissions"
+            })
+        }
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({
+            success: false,
+            message: 'Internal server error'
+        });
     }
+
 });
 
 // Endpunkt, welcher einen neuen Task erstellt und diesen zurück gibt
 app.post('/tasks', (req, res) => {
-    if (req.session.auth) {
-        const newTask = req.body;
-        newTask.Id = tasks.length + 1
-        if (newTask.Titel === "") {
-            return res.status(400).json({ message: 'Task-Titel darf nicht leer sein' })
+    try {
+        if (req.session.auth) {
+            const newTask = req.body;
+            newTask.Id = tasks.length + 1
+            if (newTask.Titel === "") {
+                return res.status(400).json({ message: 'Task-Titel darf nicht leer sein' })
+            } else {
+                tasks.push(newTask);
+                res.status(201).json(newTask)
+            }
         } else {
-            tasks.push(newTask);
-            res.status(201).json(newTask)
+            res.status(403).json({
+                message: "You don't have permsissions"
+            })
         }
-    } else {
-        res.status(403).json({
-            message: "You don't have permsissions"
-        })
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({
+            success: false,
+            message: 'Internal server error'
+        });
     }
+
 });
 
 
 // Endpunkt, welcher einen einzelnen Task zurück gibt
 app.get('/tasks/:id', (req, res) => {
-    const id = req.params.id;
-    const findTask = tasks.find(findTask => findTask.id === parseInt(id));
-    if (req.session.auth) {
-        if (findTask) {
-            res.status(200).json(findTask);
+    try {
+        const id = req.params.id;
+        const findTask = tasks.find(findTask => findTask.id === parseInt(id))
+        if (req.session.auth) {
+            if (findTask) {
+                res.status(200).json(findTask);
+            } else {
+                res.status(404).json({ message: `Task mit ID ${id} nicht gefunden` });
+            }
         } else {
-            res.status(404).json({ message: `Task mit ID ${id} nicht gefunden` });
+            res.status(403).json({
+                message: "You don't have permsissions"
+            })
         }
-    } else {
-        res.status(403).json({
-            message: "You don't have permsissions"
-        })
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({
+            success: false,
+            message: 'Internal server error'
+        });
     }
+
 });
 
 // Endpunkt, welcher den bestehenden Task verändert und diesen zurück gibt
 app.patch('/tasks/:id', (req, res) => {
-    const id = req.params.id
-    const taskToUpdate = req.body
-    const findTask = tasks.find(task => task.id === parseInt(id))
-    if (req.session.auth) {
-        if (taskToUpdate.Titel === "") {
-            return res.status(400).json({ message: 'Task-Titel darf nicht leer sein' })
-        }
-        if (findTask) {
-            tasks.splice(findTask, 1, taskToUpdate);
-            res.status(200).json(taskToUpdate);
+    try {
+        const id = req.params.id
+        const taskToUpdate = req.body
+        const findTask = tasks.find(task => task.id === parseInt(id))
+        if (req.session.auth) {
+            if (taskToUpdate.Titel === "") {
+                return res.status(400).json({ message: 'Task-Titel darf nicht leer sein' })
+            }
+            if (findTask) {
+                tasks.splice(findTask, 1, taskToUpdate);
+                res.status(200).json(taskToUpdate);
+            } else {
+                res.status(404).json({ message: `Task mit ID ${id} nicht gefunden` })
+            }
         } else {
-            res.status(404).json({ message: `Task mit ID ${id} nicht gefunden` })
+            res.status(403).json({
+                message: "You don't have Permissions"
+            });
         }
-    } else {
-        res.status(403).json({
-            message: "You don't have Permissions"
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({
+            success: false,
+            message: 'Internal server error'
         });
     }
+
 });
 
 // Endpunkt, welcher den bestehenden Task löscht
 app.delete('/tasks/:id', (req, res) => {
-    const id = req.params.id
-    const findTask = tasks.find(findTask => findTask.id === parseInt(id))
-    if (req.session.auth) {
-        if (findTask) {
-            res.status(200).json(findTask)
-            tasks.splice(findTask, 1)
+    try {
+        const id = req.params.id
+        const findTask = tasks.find(findTask => findTask.id === parseInt(id))
+        if (req.session.auth) {
+            if (findTask) {
+                res.status(200).json(findTask)
+                tasks.splice(findTask, 1)
+            } else {
+                res.status(404).json({ message: `Task mit id ${id} nicht gefunden` })
+            }
         } else {
-            res.status(404).json({ message: `Task mit id ${id} nicht gefunden` })
+            res.status(403).json({
+                message: "You don't have permsissions"
+            })
         }
-    } else {
-        res.status(403).json({
-            message: "You don't have permsissions"
-        })
     }
+    catch (err) {
+        console.error(err);
+        return res.status(500).json({
+            success: false,
+            message: 'Internal server error'
+        });
+    }
+});
 
+// Abfangen von Anfragen an nicht existierende Endpunkte
+app.use((req, res) => {
+    if (!['/login', '/verify', '/logout', '/tasks', '/tasks/:id']) {
+        return res.status(404).json({ message: 'Endpoint not found' });
+    }
 });
 
 app.listen(port, () => {
